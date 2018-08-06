39647a241c2049bc1d391a20c3048e9e734c7ace
JosephJvB <joevanbo@pm.me>
2018-08-06 14:35:52 +1200

    mission(failed): we'll gettem next time

--- client.js
+++ client.js
@@ -1,28 +1,24 @@
 const render = require('react-dom').render
 const h = require('react-hyperscript')
 
-document.addEventListener('DOMContentLoaded', () => {
-	render(
-		enhancer({
-			component: Client,
-			nextProps: {
-				test: 'success'
-			}
-		}),
-		document.getElementById('here')
-	)	
-})
-
-const Client = (props) => h('div', {}, [
+const testComponent = (props) => h('div', {}, [
 	h('h1', {}, 'Coming to you live'),
 	h('h3', {}, 'PROPS: '),
 	h('ul', {}, [
-		props && Object.keys(props).map(prop => h('li', {key: prop}, prop + ': ' + props[prop]))
+		props && Object.keys(props).map(prop => h('li', { key: prop }, prop + ': ' + props[prop]))
 	])
 ])
 
-const enhancer = ({component, nextProps}) => {
-// I want to use spread-operator here but i dont have es6 polyfills feelsbadman
-	const enhancedProps = Object.assign({}, component.props, nextProps)
+const enhancer = (firstProps) => (component) => {
+	const enhancedProps = Object.assign({}, firstProps, component.props)
 	return h(component, enhancedProps)
 }
+
+const ReactRoot = enhancer({first: 'success'})(testComponent)
+
+document.addEventListener('DOMContentLoaded', () => {
+	render(
+		h(ReactRoot, {second: 'success'}),
+		document.getElementById('here')
+	)	
+})
